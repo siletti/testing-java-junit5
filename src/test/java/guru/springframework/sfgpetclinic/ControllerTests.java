ee2829f5839484f63de9c70ced5e699119b8790c
Alberto Siletti <alberto.siletti@gmail.com>
2019-06-23 09:31:54 +0200

    lecture 65

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("controllers")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTests {
+    @BeforeAll
+    default void beforeAllDemoMethod() {
+        System.out.println("ControllerTests.beforeAllDemoMethod");
+    }
 }
